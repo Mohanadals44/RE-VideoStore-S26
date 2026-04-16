99837ce794390e3c26f30175a999e1a152c7c915
melsharif2 <melsharif2@huskers.unl.edu>
2026-04-16 02:14:45 -0500

    created 14 new test cases for rental.py code

--- VideoStore/docs/question2.md
+++ VideoStore/docs/question2.md
@@ -1,25 +1,24 @@
 ## Question 2
 Before refactoring, unit tests need to be set up.  Most of this is completed, but the unit testing for the class ```Rental``` is not done.  Finish this, following the examples from the other unit tests.  At this point you may also add test cases to the other unit tests.
 
-**Start Day/Time:**  enter date and time here
+**Start Day/Time:**  start: 04/16/1:51 am 
 
 **How did you decide whether the AI’s output was useful, accurate, or appropriate?**
 
-write answer here
+I double checked the already given code in renal.get_charge and i tried to look into the new generated code to see if it matched the logic as we are building off that. The code ended up folllwing the same logic hence i decided that it is usefull
 
 **What changes did you make to the AI‑generated material? Be specific.**
 
-write answer here
+I have not made any changes to the code
 
 **I copied my AI-assisted chat log to ./docs/log2.txt**
-[ ] Yes [ ] No 
+[ yes] Yes [ ] No 
 
 **Summarize how many test cases you added for Rental and how many extra ones you added for the rest.**
 
-write answer here
-
+I have added 14 test cases for rental and i havent added any extras
 **Difficulty Level:**
-[ ] Easy  [ ] Avg  [ ] Difficult
+[ yes ] Easy  [ ] Avg  [ ] Difficult
 
-**End Day/Time:** enter date and time here
+**End Day/Time:** 04/16/2:15 am 
 
--- VideoStore/src/Rental_t.py
+++ VideoStore/src/Rental_t.py
@@ -1,18 +1,83 @@
 """
 Rental_t.py
 
-Test program for class Rental
+Test program for class Rental.
 
 """
 
-# Assuming the Rental class is defined in a module named Rental
+import unittest
 from Rental import Rental
+from Movie import Movie
 
-def main():
-    # TODO: Add test cases
-    pass
+class TestRental(unittest.TestCase):
+
+    def test_get_movie(self):
+        movie = Movie("A", Movie.REGULAR)
+        rental = Rental(movie, 3)
+        self.assertIs(rental.get_movie(), movie)
+        self.assertEqual(rental.get_movie().get_title(), "A")
+        self.assertEqual(rental.get_movie().get_price_code(), Movie.REGULAR)
+
+    def test_get_days_rented(self):
+        movie = Movie("A", Movie.REGULAR)
+        rental = Rental(movie, 5)
+        self.assertEqual(rental.get_days_rented(), 5)
+
+    def test_get_days_rented_one_day(self):
+        movie = Movie("A", Movie.REGULAR)
+        rental = Rental(movie, 1)
+        self.assertEqual(rental.get_days_rented(), 1)
+
+    # --- REGULAR charge tests ---
+
+    def test_charge_regular_one_day(self):
+        rental = Rental(Movie("A", Movie.REGULAR), 1)
+        self.assertEqual(rental.get_charge(), 2)
+
+    def test_charge_regular_two_days(self):
+        rental = Rental(Movie("A", Movie.REGULAR), 2)
+        self.assertEqual(rental.get_charge(), 2)
+
+    def test_charge_regular_three_days(self):
+        rental = Rental(Movie("A", Movie.REGULAR), 3)
+        self.assertEqual(rental.get_charge(), 3.5)
+
+    def test_charge_regular_five_days(self):
+        rental = Rental(Movie("A", Movie.REGULAR), 5)
+        self.assertEqual(rental.get_charge(), 6.5)
+
+    # --- NEW_RELEASE charge tests ---
+
+    def test_charge_new_release_one_day(self):
+        rental = Rental(Movie("A", Movie.NEW_RELEASE), 1)
+        self.assertEqual(rental.get_charge(), 3)
+
+    def test_charge_new_release_two_days(self):
+        rental = Rental(Movie("A", Movie.NEW_RELEASE), 2)
+        self.assertEqual(rental.get_charge(), 6)
+
+    def test_charge_new_release_five_days(self):
+        rental = Rental(Movie("A", Movie.NEW_RELEASE), 5)
+        self.assertEqual(rental.get_charge(), 15)
+
+    # --- CHILDRENS charge tests ---
+
+    def test_charge_childrens_one_day(self):
+        rental = Rental(Movie("A", Movie.CHILDRENS), 1)
+        self.assertEqual(rental.get_charge(), 1.5)
+
+    def test_charge_childrens_three_days(self):
+        rental = Rental(Movie("A", Movie.CHILDRENS), 3)
+        self.assertEqual(rental.get_charge(), 1.5)
+
+    def test_charge_childrens_four_days(self):
+        rental = Rental(Movie("A", Movie.CHILDRENS), 4)
+        self.assertEqual(rental.get_charge(), 3.0)
+
+    def test_charge_childrens_six_days(self):
+        rental = Rental(Movie("A", Movie.CHILDRENS), 6)
+        self.assertEqual(rental.get_charge(), 6.0)
+
 
 if __name__ == "__main__":
-    main()
-
-
+    unittest.main()
