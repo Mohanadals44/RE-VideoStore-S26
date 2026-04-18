bed2292df7312cc80dfa70f35a1b1e0e5eabef0f
melsharif2 <melsharif2@huskers.unl.edu>
2026-04-17 23:55:05 -0500

    finished q4

--- VideoStore/docs/question4.md
+++ VideoStore/docs/question4.md
@@ -3,19 +3,19 @@ Loops that do more than one thing at a time are more difficult to comprehend and
 
 **Start Day/Time:**  April 17 11:40
 
-**How did you decide whether the AI’s output was useful, accurate, or appropriate?**
+**How did you decide whether the AI's output was useful, accurate, or appropriate?**
 
-write answer here
+I verified that the new method correctly summed up charges by looping through all rentals and calling get each on each, which matches what the variable was doing before. I also ran all existing unit tests
 
 **What changes did you make to the AI‑generated material? Be specific.**
 
-write answer here
+I have not done any changes for the output
 
 **I copied my AI-assisted chat log to ./docs/log4.txt**
-[ ] Yes [ ] No 
+[yes ] Yes [ ] No 
 
 **Difficulty Level:**
-[ ] Easy  [ ] Avg  [ ] Difficult
+[yes ] Easy  [ ] Avg  [ ] Difficult
 
 **End Day/Time:** enter date and time here
-
+ APR 17 11:54pm
--- VideoStore/src/Customer.py
+++ VideoStore/src/Customer.py
@@ -17,9 +17,14 @@ class Customer:
     def add_rental(self, rental: Rental):
         self.rentals.append(rental)
 
+    def _get_total_charge(self) -> float:
+        total = 0.0
+        for rental in self.rentals:
+            total += rental.get_charge()
+        return total
+
     def statement(self) -> str:
         result = f"Rental Record for {self.get_name()}\n"
-        total_amount = 0.0
         frequent_renter_points = 0
 
         for rental in self.rentals:
@@ -27,10 +32,9 @@ class Customer:
 
             # Title of rental
             result += f"\t{rental.get_movie().get_title()}\t" + f"{rental.get_charge():.2f}".rstrip('0').rstrip('.') + "\n"
-            total_amount += rental.get_charge()
 
         # Total amount owed
-        result += f"Amount owed is: {total_amount:.2f}".rstrip('0').rstrip('.') + "\n"
+        result += f"Amount owed is: {self._get_total_charge():.2f}".rstrip('0').rstrip('.') + "\n"
 
         # Frequent renter points earned
         result += f"You earned: {frequent_renter_points} frequent renter points\n"
